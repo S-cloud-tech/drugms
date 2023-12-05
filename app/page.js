9e2666a62f0d82f5d1b2d666a01b2b4680110f70
_enyinnaya_ <74994909+qwerty0252@users.noreply.github.com>
2023-12-05 16:53:37 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,6 +11,13 @@ export const Home = () => {
     //
     <div>
       {!loggedIn && (
+        <h1 className="head_text text-center">
+          Drug monitoring system
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center">Login</span>
+        </h1>
+      )}
+      {!loggedIn && (
         <div className="mt-1 w-full max-w-3xl flex flex-col gap-6 glassmorphism">
           <Login setLogged={setLogged} />
         </div>
