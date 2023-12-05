d5eda7eaf164df9c44cbb1548520a8b0046e642a
_enyinnaya_ <74994909+qwerty0252@users.noreply.github.com>
2023-12-05 16:24:34 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,22 @@
+"use client";
 import Dashboard from "@components/Dashboard";
-// import Feed from "@components/Feed";
-import React from "react";
+import Login from "@components/Login";
+import React, { useState } from "react";
 
 export const Home = () => {
+  const [loggedIn, setLogged] = useState(false);
   return (
     // <section className="w-full flex-center flex-col">
     // <Feed />
-    <Dashboard />
+    //
+    <div>
+      {!loggedIn && (
+        <div className="mt-1 w-full max-w-3xl flex flex-col gap-6 glassmorphism">
+          <Login setLogged={setLogged} />
+        </div>
+      )}
+      {loggedIn && <Dashboard />}
+    </div>
     // <Home />
     // </section>
   );
